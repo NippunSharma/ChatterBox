1e9f9bff897183b36dde52e0e7f4c4c496df19db
NippunSharma <inbox.nippun@gmail.com>
2021-01-02 23:31:30 +0530

    added to data.py, changed intents.json, added xTrain.pickle and yTrain.pickle

--- data.py
+++ data.py
@@ -1,1 +1,22 @@
+# This file contains all the data preprocessing tasks.
+import json
+import numpy as np
+import pickle
 
+data = None
+with open("intents.json") as file:
+    data = json.load(file)
+file.close()
+
+data = dict(data)
+
+labels = [x["tag"] for x in data["intents"] for _ in range(len(x["patterns"]))]
+training_sentences = [y for x in data["intents"] for y in x["patterns"]]
+
+with open("yTrain.pickle", "wb") as file:
+    pickle.dump(labels, file)
+file.close()
+
+with open("xTrain.pickle", "wb") as file:
+    pickle.dump(training_sentences, file)
+file.close()
